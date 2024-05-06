30ddf10f9365b2132a257ecdba0a809c069682f8
Elisandrof07 <f07nobru@gmail.com>
2024-05-06 16:29:53 +0100

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Rubik } from "next/font/google";
 import "@/styles/globals.css";
 import NextTopLoader from 'nextjs-toploader';
+import { Analytics } from "@vercel/analytics/react"
 
 
 const inter = Rubik({ subsets: ["latin"] });
